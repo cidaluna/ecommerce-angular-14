096521bdcaa9ebad9aacca8eb321e1092304e4ce
Cida Luna <cidalunna@hotmail.com>
2023-12-16 16:58:28 -0300

    created home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
