b26b7c28aba4635baf531171b3363f0aa85e711a
Cida Luna <cidalunna@hotmail.com>
2023-12-16 21:04:38 -0300

    Added category filter

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css']
+})
+export class FiltersComponent implements OnInit {
+  categories = ['shoes', 'sports'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
