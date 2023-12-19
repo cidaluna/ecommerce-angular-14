bb561274b082251eb3b16eaf8cd5b320c5ca657a
Cida Luna <cidalunna@hotmail.com>
2023-12-19 13:55:47 -0300

    Implemented the expected behavior to remove items from the cart until it reaches zero

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -64,4 +64,8 @@ export class CartComponent implements OnInit {
     this.cartService.addToCart(item);
   }
 
+  onRemoveQuantity(item: CartItem): void{
+    this.cartService.removeQuantity(item);
+  }
+
 }
